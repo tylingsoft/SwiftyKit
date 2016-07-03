d201959f39175a3ed4ac01d5817c4b1cd02aa281
Tyler Long <tyler4long@gmail.com>
2016-07-03 12:39:59 +0800

    Initial Commit

---
+++ SwiftyKit/SwiftyKit.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyKit.h
+//  SwiftyKit
+//
+//  Created by Tyler Long on 7/3/16.
+//  Copyright © 2016 Tylingsoft. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftyKit.
+FOUNDATION_EXPORT double SwiftyKitVersionNumber;
+
+//! Project version string for SwiftyKit.
+FOUNDATION_EXPORT const unsigned char SwiftyKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyKit/PublicHeader.h>
+
+
